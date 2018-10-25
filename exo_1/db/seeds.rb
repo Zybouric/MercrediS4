40be09787dbe432997ef9fa223340cd532d0ade6
Zybouric <43443663+Zybouric@users.noreply.github.com>
2018-10-25 02:21:10 +0200

    update seed but there is still some issues

--- exo_1/db/seeds.rb
+++ exo_1/db/seeds.rb
@@ -8,6 +8,20 @@
 #10.times do |index|
 #	user = User.create!(first_name: "first_name#{index}", last_name: "last_name#{index}",email: "email#{index}@example.com")
 #	articles = Article.create!(title: "title#{index}", content: "content#{index}",user_id: user.id,category_id: category.id)
-#end
+#end	
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+#5.times do
+ # category = Category.create!(name: Faker::Simpsons.character)
+#end
+#10.times do
+ #user = User.create!(first_name: Faker::LordOfTheRings.character, last_name: Faker::Beer.name, email: Faker::Internet.email)
+#got fake name and emai
+  articles = Article.create!(title: Faker::Movie.quote, content: Faker::Hipster.sentences, user_id: Faker::Number.between(User.first.id,User.last.id), category_id: Faker::Number.between(User.first.id,User.last.id))
+#end
+
+15.times do
+  comment = Comment.create!(comment: Faker::ChuckNorris.fact, user_id: Faker::Number.between(User.first.id,User.last.id), article_id: Faker::Number.between(User.first.id,User.last.id))
+  likes = Likes.create!(user_id: Faker::Number.between(User.first.id,User.last.id), article_id: Faker::Number.Faker::Number.between(User.first.id,User.last.id))
+end
